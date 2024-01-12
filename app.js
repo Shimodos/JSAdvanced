3ad66fda1874addc67c4e97801a2f96e378efc33
Alekssey Demchenko <ad@pesochin.net>
2024-01-12 20:28:01 +0200

    JSAdvanced: 3.14 Fast create Map

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 'use strict';
-const weatherMap = new Map();
-weatherMap.set('Valencia', '13').set('Madrid', '10');
 
-console.log(weatherMap.get('Valencia')); // 13
-console.log(weatherMap.get('not found')); // undefined
-
-console.log(weatherMap.has('Valencia')); // true
-console.log(weatherMap.has('not found')); // false
-
-console.log(weatherMap.delete('Valencia')); // true
-weatherMap.clear(); // clears all the map
-
-const arr = [1, 2, 3];
-weatherMap.set(1, 5).set(true, 'yes').set(false, 'no').set(arr, 'array').set({ a: 1 }, 'object');
+const weatherMap = new Map([
+  ['rainy', 'Bring an umbrella'],
+  ['sunny', 'Dress lightly'],
+  ['cloudy', 'Go outside'],
+  ['snowing', 'Wear boots'],
+]);
 
 console.log(weatherMap);
-console.log(weatherMap.size); // 5
 
-console.log(weatherMap.get(arr));
-console.log(weatherMap.get({ a: 1 })); // undefined
+const weatherObj = {
+  Valencia: 10,
+  Madrid: 20,
+  Barcelona: 30,
+};
+
+console.log(Object.entries(weatherObj));
+const weatherMap2 = new Map(Object.entries(weatherObj));
+console.log(weatherMap2);
