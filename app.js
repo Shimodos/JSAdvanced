cae533a394c00bb35d4f279737a738e20b3a5de9
Alekssey Demchenko <ad@pesochin.net>
2024-01-13 23:43:30 +0200

    JSAdvanced: 3.18 WeakSet

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
 'use strict';
-
-let a = { a: 1 };
-let b = { b: 1 };
-const map = new WeakMap();
-map.set(a, 'one');
-map.set(b, 'two');
-console.log(map);
-console.log(map.get(a));
-console.log(map.has(a));
-console.log(map);
-
+let a = { x: 1 };
+let b = { y: 2 };
+const set = new WeakSet([a, b]);
 a = null;
-setTimeout(() => {
-  console.log(map);
-}, 1000);
-
-let cache = new WeakMap();
-
-function getValue(obj) {
-  if (!cache.has(obj)) {
-    let value = 1; // сложные вычисления для value
-    cache.set(obj, value);
-  }
-  return cache.get(obj);
-}
-
-const res = getValue(b);
-console.log(res);
-const res2 = getValue(b);
-console.log(res2);
+setTimeout(() => console.log(set), 1000);
