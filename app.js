59eec8a6f52292447ad5682937b7329e7e0e167f
Alekssey Demchenko <ad@pesochin.net>
2024-01-15 19:21:20 +0200

    JSAdvanced: 4.23 remainder operator

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 'use strict';
 
-console.log(Math.sqrt(36)); // 6
-console.log(Math.cbrt(27));
-console.log(16 ** (1 / 4)); // 2
+console.log(Math.round(1.6)); // 2
 
-console.log(Math.sign(-10)); // -1
-console.log(Math.sign(35)); // 1
-console.log(Math.abs(-100)); // 100
+console.log(Math.ceil(1.1)); // 2
 
-console.log(Math.exp(1)); // 2.718281828459045
+console.log(Math.floor(1.9)); // 1
 
-//min and max
-console.log(Math.min(1, 2, 3, 4, 5, 6, 7, 8, 9)); // 1
-console.log(Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9)); // 9
+console.log(Math.trunc(1.75)); // 1
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(Math.min(...arr)); // 1
+console.log((1.6434).toFixed(2));
+console.log(Number((1.6434).toFixed(2)));
 
-//random
+//функция , которая принимает min и max и возвращает случайное число между ними  и округляет его
 
-console.log(Math.trunc(Math.random() * 6) + 1); // 1 to 6
+function randomInteger(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+console.log(randomInteger(1, 20));
+
+// остаток от деления
+
+console.log(5 % 2); // 1
+console.log(4 % 2); // 0
+console.log(3 % 2); // 1
+console.log(2 % 2); // 0
+
+// проверка на четность
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(5)); // false
+console.log(isEven(4)); // true
