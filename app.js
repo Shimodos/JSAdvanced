439cca362de01867d11015d08e5ba448db530326
Alekssey Demchenko <ad@pesochin.net>
2024-01-22 18:56:33 +0200

    JSAdvanced: 8.46 Static

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 'use strict'; // strict mode
 
-const task = {
-  title: 'My Title',
-  dueTo: new Date('2024/01/26'),
-
-  get isOverdue() {
-    return this.dueTo < Date.now();
-  },
-
-  set isOverdue(value) {
-    if (!value) {
-      this.dueTo = new Date();
-    }
-  },
-};
-
-console.log(task.isOverdue); // false
-task.isOverdue = false;
-console.log(task);
-
-class Task {
-  constructor(title, dueDate) {
-    this.title = title;
-    this.dueDate = dueDate;
+class Test {
+  static a = 1;
+  static hello() {
+    console.log('Hello');
   }
 
-  get isOverdue() {
-    return this.dueTo < Date.now();
-  } // получаем значение свойства
-
-  set dueDate(date) {
-    if (date < new Date()) {
-      return;
-    }
-    this._dueDate = date; // устанавливаем значение свойства
+  static {
+    let b = 5;
+    this.a = b;
+    console.log('Static block');
   }
 }
-
-const newTask = new Task('My Title', new Date('2024/01/26'));
-console.log((newTask.dueDate = new Date('2024/02/02')));
-console.log(newTask);
+Test.hello(); // Hello
+console.log(Test.a); // 1
