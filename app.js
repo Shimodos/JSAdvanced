bc1708e53c83ac7cbec8ad67974ea0759394c2f7
Alekssey Demchenko <ad@pesochin.net>
2024-01-24 18:19:09 +0200

    JSAdvanced: 8.48_1 Test Class

--- app.js
+++ app.js
@@ -1,55 +1,23 @@
 'use strict'; // strict mode
 
-/* реализовать класс пользователя, сосвойстыввами логин, пароль, почта
- */
+const User = {
+  init(email, pass) {
+    this.email = email;
+    this.pass = pass;
+  },
+  log() {
+    console.log('User logged!');
+  },
+};
 
-class User {
-  // private login;
-  // private password;
-  // private email;
+const user = Object.create(User);
 
-  #login;
-  #_password;
-  #email;
-
-  constructor(login, password, email) {
-    this.#login = login;
-    this.#password = password;
-    this.#email = email;
-  }
-
-  set #password(pass) {
-    this.#_password = pass.split('').reverse().join(''); // reverse password
-  }
-
-  get #password() {
-    return this.#_password.split('').reverse().join('');
-  }
-
-  get login() {
-    return this.#login;
-  }
-
-  get email() {
-    return this.#email;
-  }
-
-  checkPassword(pass) {
-    return this.#password === pass;
-  }
-
-  changePassword(oldPas, newPass) {
-    if (!this.checkPassword(oldPas)) {
-      return false;
-    }
-    this.#password = newPass;
-    return true;
-  }
-}
-
-const user = new User('Alex', '12345', 'w@w.com');
+User.log(); // User logged!
+console.log(user); // {}
+user.log(); // User logged!
+user.init('w@w.com', '123456');
 console.log(user);
-console.log(user.checkPassword('12345'));
-console.log(user.changePassword('12345', '54321'));
-console.log(user);
-console.log(user.email);
+console.log(user.__proto__ == User); // true
+
+const admin = Object.create(user);
+console.log(admin); // {}
