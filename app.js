3951d6788044afd0cb7e21bfee166c844753d40f
Alekssey Demchenko <ad@pesochin.net>
2024-01-25 19:41:50 +0200

    JSAdvanced: 9.51 Inheritance

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
 'use strict'; // strict mode
 
-/* Абстракиця vs Инкапсуляция
-  - Назначение
-  - Режисер
-  - Актеры
-  - Сценарий
-  - Съемочная группа
-*/
+const Book = function (title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+};
 
-class Movie {
-  #name;
-  #actors;
-  #director;
-  rating;
+Book.prototype.buy = function () {
+  console.log(`You bought: ${this.title}`);
+}; // Добавили метод buy в прототип Book
 
-  constructor(name, actors, director) {
-    this.#name = name;
-    this.#actors = actors;
-    this.#director = director;
-  }
+const AudioBook = function (title, author, lenMin) {
+  Book.call(this, title, author);
+  this.lenMin = lenMin;
+}; // Создали конструктор AudioBook
 
-  get name() {
-    return this.#name;
-  }
+AudioBook.prototype = Object.create(Book.prototype); // Отнаследовались от Book и создали новый прототип
+AudioBook.prototype.constructor = AudioBook; // Указали конструктор
 
-  get actors() {
-    return this.#actors;
-  }
+AudioBook.prototype.log = function () {
+  console.log(` ${this.title} - ${this.lenMin} minutes`);
+}; // Добавили метод log в прототип AudioBook
 
-  get director() {
-    return this.#director;
-  }
-}
+const book1 = new Book('Book One', 'John Doe', '2013');
+const audioBook1 = new AudioBook('Audio Book One', 'John Doe', 20 * 60);
 
-const movie = new Movie('Titanic', ['Leonardo DiCaprio', 'Kate Winslet'], 'James Cameron');
+book1.buy();
+audioBook1.log();
+console.log(book1);
+console.log(audioBook1);
 
-console.log(movie);
+console.log(audioBook1 instanceof AudioBook);
+console.log(audioBook1 instanceof Book);
