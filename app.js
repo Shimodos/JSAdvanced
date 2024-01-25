20c1237d961b991dd4855fde9caaf059fc7e401c
Alekssey Demchenko <ad@pesochin.net>
2024-01-25 23:20:07 +0200

    JSAdvanced: 9.52 Inheritance ES6

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
 'use strict'; // strict mode
 
-const Book = function (title, author, year) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-};
+// const Book = function (title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// };
 
-Book.prototype.buy = function () {
-  console.log(`You bought: ${this.title}`);
-}; // Добавили метод buy в прототип Book
+// Book.prototype.buy = function () {
+//   console.log(`You bought: ${this.title}`);
+// }; // Добавили метод buy в прототип Book
 
-const AudioBook = function (title, author, lenMin) {
-  Book.call(this, title, author);
-  this.lenMin = lenMin;
-}; // Создали конструктор AudioBook
+// const AudioBook = function (title, author, lenMin) {
+//   Book.call(this, title, author);
+//   this.lenMin = lenMin;
+// }; // Создали конструктор AudioBook
 
-AudioBook.prototype = Object.create(Book.prototype); // Отнаследовались от Book и создали новый прототип
-AudioBook.prototype.constructor = AudioBook; // Указали конструктор
+// AudioBook.prototype = Object.create(Book.prototype); // Отнаследовались от Book и создали новый прототип
+// AudioBook.prototype.constructor = AudioBook; // Указали конструктор
 
-AudioBook.prototype.log = function () {
-  console.log(` ${this.title} - ${this.lenMin} minutes`);
-}; // Добавили метод log в прототип AudioBook
+// AudioBook.prototype.log = function () {
+//   console.log(` ${this.title} - ${this.lenMin} minutes`);
+// }; // Добавили метод log в прототип AudioBook
+
+// const book1 = new Book('Book One', 'John Doe', '2013');
+// const audioBook1 = new AudioBook('Audio Book One', 'John Doe', 20 * 60);
+
+// book1.buy();
+// audioBook1.log();
+// console.log(book1);
+// console.log(audioBook1);
+
+// console.log(audioBook1 instanceof AudioBook);
+// console.log(audioBook1 instanceof Book);
+
+// ES6 Classes
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  } // Конструктор
+
+  buy() {
+    console.log(`You bought: ${this.title} - ${this.author} - ${this.year}`);
+  } // Метод
+}
+
+class AudioBook extends Book {
+  constructor(title, author, lenMin) {
+    super(title, author);
+    this.lenMin = lenMin;
+  }
+
+  log() {
+    console.log(`You bought: ${this.title} - ${this.lenMin} minutes`);
+  }
+} // Конструктор AudioBook наследуется от Book
 
 const book1 = new Book('Book One', 'John Doe', '2013');
 const audioBook1 = new AudioBook('Audio Book One', 'John Doe', 20 * 60);
 
 book1.buy();
 audioBook1.log();
-console.log(book1);
-console.log(audioBook1);
-
-console.log(audioBook1 instanceof AudioBook);
-console.log(audioBook1 instanceof Book);
