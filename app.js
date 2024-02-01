3e634c291a1a6196ce1a60c420910c9014ce8fb9
Alekssey Demchenko <ad@pesochin.net>
2024-02-01 18:55:55 +0200

    JSAdvanced: 12.73 Promises: Reject processing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 'use strict'; // strict mode
 
 // Promise is a JavaScript object for asynchronous operation.
+// Chain
+
 fetch('https://dummyjson.com/products')
+  .then(
+    (res) => res.json(),
+    // (error) => console.log('error1'),
+  )
+  .then(
+    ({ products }) => {
+      console.log(products);
+      return fetch('https://dummyjson.com/products/' + products[1].id);
+    },
+    // (error) => console.log('error2'),
+  )
   .then((res) => res.json())
-  .then(({ products }) => {
-    console.log(products);
-    return fetch('https://dummyjson.com/products/' + products[1].id);
-  })
-  .then((res) => res.json())
-  .then((product) => console.log(product));
+  .then((product) => console.log(product))
+  .catch((error) => console.log(error)); // error handling
