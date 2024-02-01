d2a2a74639d9491e4ca789a7201d29a98003df73
Alekssey Demchenko <ad@pesochin.net>
2024-02-01 18:44:01 +0200

    JSAdvanced: 12.72 Promises: promise chain

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict'; // strict mode
 
 // Promise is a JavaScript object for asynchronous operation.
-
-const res = fetch('https://jsonplaceholder.typicode.com/todos/1');
-console.log(res); // pending
-
-// Три состояния промиса: pending(ожидание) и fulfilled(выполнено) и rejected(отклонено)
+fetch('https://dummyjson.com/products')
+  .then((res) => res.json())
+  .then(({ products }) => {
+    console.log(products);
+    return fetch('https://dummyjson.com/products/' + products[1].id);
+  })
+  .then((res) => res.json())
+  .then((product) => console.log(product));
