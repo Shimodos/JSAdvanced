fba9a4f58444ce27d0f4d886c00c3e22def4d785
Alekssey Demchenko <ad@pesochin.net>
2024-02-01 19:09:35 +0200

    JSAdvanced: 12.75 Promises: Select category selection

--- app.js
+++ app.js
@@ -3,20 +3,25 @@
 // Promise is a JavaScript object for asynchronous operation.
 // Chain
 
-fetch('https://dummyjson.com/products1')
-  .then(
-    (res) => res.json(),
-    // (error) => console.log('error1'),
-  )
-  .then(
-    ({ products }) => {
-      console.log(products);
-      return fetch('https://dummyjson.com/products/' + products[1].id);
-    },
-    // (error) => console.log('error2'),
-  )
+fetch('https://dummyjson.com/products/categories')
   .then((res) => res.json())
-  .then((product) => console.log(product))
+  .then((categories) => {
+    console.log(categories);
+    return fetch('https://dummyjson.com/products/categories/' + categories[1].id);
+  });
 
-  .catch((error) => console.log(error)) // error handling
-  .finally(() => console.log('finally')); // finally
+function createSelect(arr) {
+  const el = document.querySelector('.filter');
+  el.innerHTML = `<select>
+  ${arr.map((item) => `<option value=${el}>${item}</option>`)}
+  </select>`;
+}
+
+function getCategories() {
+  fetch('https://dummyjson.com/products/categories')
+    .then((res) => res.json())
+    .then((data) => createSelect(data))
+    .catch((err) => console.log(`Error: ${err}`));
+}
+
+getCategories();
