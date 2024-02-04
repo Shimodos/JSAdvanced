019b05b1acf05c876a058a4a5cc57ff8a4c12bfd
Alekssey Demchenko <ad@pesochin.net>
2024-02-04 17:40:35 +0200

    JSAdvanced: 13.80 Event Loop: Creating a Simple Promise

--- app.js
+++ app.js
@@ -2,17 +2,31 @@
 
 // Event Loop
 
-console.log(1)
+const prom = new Promise((resolve, reject) => {
+  if ( new Date() < new Date('2024-06-20') ) {
+    reject(new Error('Promise rejected'));
+  }
+  resolve('Promise resolved');
+});
 
-setTimeout(() => {
-  console.log(2)
-}, 0)
-Promise.resolve(3).then((res) => {
-  console.log(res)
-})
+prom
+    .then((res) => {console.log(res)})
+    .catch((err) => {console.log(err)})
 
-console.log(4)
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms * 1000)
+  });
+}
 
-// for (let i = 0; i < 1000000000; i++) {
-//   // do nothing
-// }
+timeout(1)
+    .then(() => {
+      console.log('1 second passed')
+      return timeout(1);
+    })
+    .then(() => {
+      console.log('2 seconds passed')
+      return timeout(1);
+    })
