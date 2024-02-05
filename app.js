8b6ad250ce807b99617eca97c6946e8701b15ebe
Alekssey Demchenko <ad@pesochin.net>
2024-02-05 22:33:27 +0200

    JSAdvanced: 14.91 Modern async JS: Activiti Generator

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 'use strict'; // strict mode
 
-async function main() {
-  const res = await fetch('https://dummyjson.com/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0',
-    },
-    body: JSON.stringify({
-      username: 'kminchelle',
-      password: '0lelplR',
-    }),
-  });
-  const data = await res.json();
-  console.log(data);
+const wrapper = document.querySelector('.wrapper');
+
+async function getActivity() {
+  const response = await fetch('http://www.boredapi.com/api/activity/');
+  const data = await response.json();
+  return data;
 }
 
-main();
+async function generate() {
+  try {
+    wrapper.innerHTML = '';
+    const data = await Promise.all([getActivity(), getActivity(), getActivity()]);
+    console.log(data);
+    for (const el of data) {
+      const element = document.createElement('div');
+      element.innerHTML = `<h2>${el.activity}</h2>`;
+      wrapper.appendChild(element);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
