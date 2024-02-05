42e4cbeebbcd53ee0c607c4374c48f3a8e38e3a0
Alekssey Demchenko <ad@pesochin.net>
2024-02-05 22:06:40 +0200

    JSAdvanced: 14.89 Modern async JS: Other Promise combinations

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ async function getProduct(id) {
   return response.json();
 }
 
+async function getProductError(id) {
+  const response = await fetch('http://dummyjson1.com/products/' + id);
+  return response.json();
+}
+
 async function main() {
-  const { products } = await getAllProducts();
-  const res = await Promise.all(
-    products.map(async (product) => {
-      return await getProduct(product.id);
-    }),
-  );
-  console.log(res);
-  // for (const product of products) {
-  //   const res = await getProduct(product.id);
-  //   console.log(res);
-  // }
+  const res1 = await Promise.all([getProduct(1), getProduct(2)]);
+  console.log(res1);
+  const res2 = await Promise.allSettled([getProduct(1), getProductError(2)]);
+  console.log(res2);
+  const res3 = await Promise.race([getProduct(1), getProduct(2)]);
 }
 
 main();
