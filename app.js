077a57d8ba6cc8fd51f45ea21178c8a54d250c60
Alekssey Demchenko <ad@pesochin.net>
2024-02-05 19:56:21 +0200

    JSAdvanced: 14.84 Modern async JS: Try catch

--- app.js
+++ app.js
@@ -2,28 +2,45 @@
 
 // Modern async/await syntax JavaScript
 
-function getProduct () {
+function getProduct() {
   fetch('https://dummyjson.com/products/')
-      .then(response => response.json())
-      .then(data => console.log(data))
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
-getProduct()
-console.log('End')
+getProduct();
+console.log('End');
 
 // Async/Await syntax
 
- async function getProduct1 () {
-  let productsResponse = await fetch('https://dummyjson.com/products/')
-  let { products } = await productsResponse.json()
-  console.log(products)
+async function getProduct1() {
+  try {
+    const productsResponse = await fetch('https://dummyjson.com/products/');
+    if (!productsResponse.ok) {
+      throw new Error(productsResponse.status);
+    }
+    const { products } = await productsResponse.json();
+    console.log(products);
 
-   const cartsResponse = await fetch('https://dummyjson.com/carts/')
-   const carts = await cartsResponse.json()
-   console.log(carts)
+    const cartsResponse = await fetch('https://dummyjson.com/carts/');
+    const carts = await cartsResponse.json();
+    console.log(carts);
 
-   let productResponse = await fetch('https://dummyjson.com/products/' +products[0].id)
-   let product = await productResponse.json()
-   console.log(product)
+    let productResponse = await fetch('https://dummyjson.com/products/' + products[0].id);
+    let product = await productResponse.json();
+    console.log(product);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('Finally');
+  }
 }
-getProduct1().then(() => console.log('End1'))
+
+try {
+  const a = 5;
+  a = 6;
+} catch (error) {
+  console.log(error);
+}
+
+getProduct1().then(() => console.log('End1'));
