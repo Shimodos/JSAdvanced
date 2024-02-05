161da7894450f5ebb39ada32d410b6d7620c6cfe
Alekssey Demchenko <ad@pesochin.net>
2024-02-05 22:13:27 +0200

    JSAdvanced: 14.90 Modern async JS: All features of fetch

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 'use strict'; // strict mode
 
-async function getAllProducts() {
-  const response = await fetch('http://dummyjson.com/products');
-  return response.json();
-}
-
-async function getProduct(id) {
-  const response = await fetch('http://dummyjson.com/products/' + id);
-  return response.json();
-}
-
-async function getProductError(id) {
-  const response = await fetch('http://dummyjson1.com/products/' + id);
-  return response.json();
-}
-
 async function main() {
-  const res1 = await Promise.all([getProduct(1), getProduct(2)]);
-  console.log(res1);
-  const res2 = await Promise.allSettled([getProduct(1), getProductError(2)]);
-  console.log(res2);
-  const res3 = await Promise.race([getProduct(1), getProduct(2)]);
+  const res = await fetch('https://dummyjson.com/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0',
+    },
+    body: JSON.stringify({
+      username: 'kminchelle',
+      password: '0lelplR',
+    }),
+  });
+  const data = await res.json();
+  console.log(data);
 }
 
 main();
