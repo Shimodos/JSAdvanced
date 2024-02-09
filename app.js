20cdc9c62081af63c7f7b592fbba9704780581e7
Alekssey Demchenko <ad@pesochin.net>
2024-02-09 18:50:34 +0200

    JSAdvanced: 15.97 DOM endEvents: Example of event bubbling

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 'use strict'; // strict mode
 
 const button = document.querySelector('.button');
+const inner = document.querySelector('.inner');
+const wrapper = document.querySelector('.wrapper');
 
-const evenHandler = function (event) {
+button.addEventListener('click', function (event) {
   console.log('Button clicked');
-};
-
-button.addEventListener('click', evenHandler);
-button.addEventListener('click', (event) => {
-  console.log('2nd event listener');
-  button.removeEventListener('click', evenHandler);
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = 'red';
 });
+
+inner.addEventListener('click', function (event) {
+  console.log('Inner clicked');
+  console.log(event.target);
+  console.log(event.currentTarget);
+  this.style.backgroundColor = 'green';
+  // event.stopPropagation(); // stop bubbling
+});
+
+wrapper.addEventListener(
+  'click',
+  function (event) {
+    console.log('Wrapper clicked');
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = 'blue';
+  },
+  true,
+); // capture phase
