32602b8dcec79810aa42490891b1e52271e53cc1
Alekssey Demchenko <ad@pesochin.net>
2024-02-14 19:58:20 +0200

    JSAdvanced: 16.107 Modularity: Await at the top level

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict'; // strict mode
 
-import { add, sub } from './calc.js';
+import { add, sub, products } from './calc.js';
 
-// console.log(add(10, 5)); // 15
-console.log(sub(10, 5)); // 15
-
-// setInterval(() => {
-//   console.log(add(10, 5));
-// }, 1000);
+console.log(add(10, 5)); // 15
+console.log(sub(10, 5)); // 5
+console.log(products);
+console.log(1);
--- calc.js
+++ calc.js
@@ -11,3 +11,13 @@ export function sub(f, s) {
 //     return f + s + 10;
 //   };
 // }, 1500);
+
+async function getProducts() {
+  const response = await fetch('https://dummyjson.com/products');
+  return response.json();
+}
+
+// await getProducts().then((data) => console.log(data));
+// console.log(await getProducts());
+
+export const products = await getProducts();
