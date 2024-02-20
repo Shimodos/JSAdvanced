959fc47617fde1bcfd8f0ebd336459aec586bf2b
Alekssey Demchenko <ad@pesochin.net>
2024-02-21 00:10:06 +0200

    JSAdvanced: 18.118 Project Books Libraries: State and reactivity

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import { MainView } from './views/main/main';
 class App {
   routes = [{ path: '', view: MainView }];
+  appState = {
+    favorites: [],
+  }; // This is where you can store the state of your app
 
   constructor() {
     window.addEventListener('hashchange', this.route.bind(this));
@@ -13,7 +16,7 @@ class App {
     }
 
     const view = this.routes.find((r) => r.path == location.hash).view;
-    this.currentView = new view();
+    this.currentView = new view(this.appState);
     this.currentView.render();
   }
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,33 @@
 import { AbstractView } from '../../common/view.js';
+import onChange from 'on-change';
 
 export class MainView extends AbstractView {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0,
+  };
+
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
+
     this.setTitle('Search books');
+  }
+
+  appStateHook(path) {
+    console.log(path);
+
+    // This is where you can add hooks to the app state
   }
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = 'Main view content goes here';
+    main.innerHTML = `Books list: ${this.appState.favorites.length}`;
     this.app.innerHTML = ''; // Clear the app
     this.app.append(main);
+    this.appState.favorites.push('test');
   }
 }
